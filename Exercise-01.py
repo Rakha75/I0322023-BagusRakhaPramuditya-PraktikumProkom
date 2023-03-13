ca475355f92a1ac2624f1c3872fe6efc404593b9
Rakha75 <bagusrakha644@student.uns.ac.id>
2023-03-13 19:59:59 +0700

    mengganti nama

--- Exercise-01.py
+++ Exercise-01.py
@@ -1,4 +1,8 @@
 print("Bagus Rakha Pramuditya")
-print("I0322023")
-print("Saya tinggal di Kabupaten Pati sebuah kabupaten yang terletak di sebelah utara provinsi Jawa Tengah.Pati adalah sebuah kota yang bisa dibilang cukup kecil dan penduduknya tidak sepadat di Solo.")
-print("Harapan saya menjadi lulusan Teknik Industri adalah saya mampu menenyelesaikan masalah yang ada di industri khususnya di industri tekstil.karena saya sangat tertarik dalam industri tersebut serta dapat mengembangkan pasar untuk industri tekstil agar lebih baik lagi")
+print("I0322023\n")
+
+print("Perkenakan nama saya Bagus Rakha Pramuditya saya lahir pada tangga 1 April 2004 di Pati. Saya adalah anak tunggal.")
+
+print("Saya tinggal di Kabupaten Pati sebuah kabupaten yang terletak di sebelah utara provinsi Jawa Tengah.Pati adalah sebuah kota yang bisa dibilang cukup kecil dan penduduknya tidak sepadat di Solo. Disana terkenal dengan makanan khasnya yaitu nasi gandul yang rasanya sangat enak dan salah satu makanan kesukaan saya. ")
+
+print("Harapan saya menjadi lulusan Teknik Industri adalah saya mampu menenyelesaikan masalah yang ada di industri khususnya di industri tekstil.karena saya sangat tertarik dalam industri tersebut serta dapat mengembangkan pasar untuk industri tekstil agar lebih baik lagi.")
