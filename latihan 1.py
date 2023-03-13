c3b4b915f5b04208a0269ccb751a067b3a76b4bf
Rakha75 <bagusrakha644@student.uns.ac.id>
2023-03-13 10:16:56 +0700

    Menambahkan Biodata dan Harapan

--- latihan 1.py
+++ latihan 1.py
@@ -1,1 +1,4 @@
-print("bgst!")
+print("Bagus Rakha Pramuditya")
+print("I0322023")
+print("Saya tinggal di Kabupaten Pati sebuah kabupaten yang terletak di sebelah utara provinsi Jawa Tengah.Pati adalah sebuah kota yang bisa dibilang cukup kecil dan penduduknya tidak sepadat di Solo")
+print("Harapan saya menjadi lulusan Teknik Industri adalah saya mampu menenyelesaikan masalah yang ada di industri khususnya di industri tekstil karena saya sangat tertarik dalam industri tersebut serta dapat mengembangkan pasar untuk industri tekstil agar lebih baik lagi")
--- latihan3.py
+++ latihan3.py
@@ -1,1 +1,2 @@
 print("Tolong saya")
+print("apalah")
