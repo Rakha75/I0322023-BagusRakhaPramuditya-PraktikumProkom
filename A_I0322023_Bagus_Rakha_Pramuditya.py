1a6713f881ef744d048501916ad816d8eb41d83a
Rakha75 <bagusrakha644@student.uns.ac.id>
2023-04-08 14:54:47 +0700

    menambahkan

--- A_I0322023_Bagus_Rakha_Pramuditya.py
+++ A_I0322023_Bagus_Rakha_Pramuditya.py
@@ -6,18 +6,12 @@ biologi = int(input("masukkan nilai biologi: "))
 matematika = int(input("masukkan nilai matematika: "))
 
 ip = (fisika + biologi+ matematika)/ 3
-if ip == 80 >=100:
-    print("A")
-elif ip == 70 >= 79.9:
-    print("B")
-elif ip == 60>=69.9:
-    print("C")
-elif ip == 50>=59.9:
-    print("D")
-elif ip == 0>=49.9:
-    print("E")
-else:
-    print("tidak")
 
-print ("mahasiswa dengan nama",nama,"dan nim",nim,"mendapatkan IP", ip)
+dict = {'A':{80>=100}, 
+                     'B':{70>=79.9},
+                     'C':{60>=69.9},
+                     'D':{50>=59.9},
+                     'E':{0>=49.9}}
+
+print ("mahasiswa dengan nama",nama,"dan nim",nim,"mendapatkan IP", ip,dict)
 
